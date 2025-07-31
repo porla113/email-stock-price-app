42faf3e119315fcf65d6ef193ed0f7d14401d76f
Por Cho <porla113@gmail.com>
2025-07-31 13:49:00 +0700

    Implement: litst_to_json in main.py, update min, max in stocks.json if price is out of range

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ from bs4 import BeautifulSoup
 from send_email import send_email
 from write_csv import write_csv
 from read_json import json_to_list
+from write_json import list_to_json
 
 # Stock list to check
-stock_list = json_to_list("stocks.json")
+stock_json = "stocks.json"
+stock_list = json_to_list(stock_json)
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 BKK_TZ = ZoneInfo('Asia/Bangkok')
 
 
-for stock in stock_list:
+for index, stock in enumerate(stock_list):
 
     # Request URL
     url = f"https://www.set.or.th/th/market/product/stock/quote/{stock["symbol"]}/price"
@@ -43,17 +45,30 @@ for stock in stock_list:
         stock_data = {
             "date": date_time,
             "price": stock_price,
+            "trend": "",
         }
-
-        # Write stock data to a csv file.
-        write_csv(file_path,stock_data)
 
         # Test
         # print("record data")
         # print(f"{stock["symbol"]}, {date_time}, {stock_price}")
 
-        # If the price is outside the threshold, send an email
-        if float(stock_price) < stock["min"] or float(stock_price) > stock["max"]:
+        # If the price is greater than the max, update max to new max (+5%) and send an email
+        if  float(stock_price) > stock["max"]:
+
+            # Calculate new max, min
+            stock["max"] = stock["max"] * 1.05
+            stock["min"] = stock["min"] * 1.05
+            stock_data["trend"] = "up"
+
+            # Update min and max in stock_list
+            stock_list[index]["max"] = stock["max"]
+            stock_list[index]["min"] = stock["min"]
+
+            # Write stock data to a csv file.
+            write_csv(file_path,stock_data)
+
+            # Write new min, max to stocks.json
+            list_to_json(stock_list, stock_json)
 
             # Prepare for email
             load_dotenv()
@@ -61,7 +76,7 @@ for stock in stock_list:
 
             # Email message
             email_subject = "stock price alert!".title()
-            email_body = f"On {date_time}, <b>{stock["symbol"]}</b> price is <b>{stock_price}</b>!"
+            email_body = f"On {date_time}, <b>{stock["symbol"]}</b> price is <b>{stock_price}</b>!<br><b>Trend:{stock_data["trend"]}</b> "
 
             # Send email
             send_email(recipient_email, email_subject, email_body, "html")
@@ -69,6 +84,40 @@ for stock in stock_list:
             # Test
             # print("sent email!")
             # print(email_body)
+
+        # If the price is smaller than the min, update max to new max (-5%) and send an email
+        elif float(stock_price) < stock["min"]:
+
+            # Calculate new max, min
+            stock["max"] = stock["max"] * 0.95
+            stock["min"] = stock["min"] * 0.95
+            stock_data["trend"] = "down"
+
+            # Update min and max in stock_list
+            stock_list[index]["max"] = stock["max"]
+            stock_list[index]["min"] = stock["min"]
+
+            # Write stock data to a csv file.
+            write_csv(file_path,stock_data)
+
+            # Write new min, max to stocks.json
+            list_to_json(stock_list, stock_json)
+
+            # Prepare for email
+            load_dotenv()
+            recipient_email = os.getenv("RECIPIENT_EMAIL")
+
+            # Email message
+            email_subject = "stock price alert!".title()
+            email_body = f"On {date_time}, <b>{stock["symbol"]}</b> price is <b>{stock_price}</b>!<br><b>Trend:{stock_data["trend"]}</b> "
+
+            # Send email
+            send_email(recipient_email, email_subject, email_body, "html")
+
+        else:
+
+        # Write stock data to a csv file.
+            write_csv(file_path,stock_data)
 
     else:
 
@@ -79,6 +128,7 @@ for stock in stock_list:
         stock_data = {
             "date": date_time,
             "price": "n/a",
+            "trend": "n/a"
         }
 
         # Write stock data to a csv file.
